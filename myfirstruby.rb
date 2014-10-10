45766ee4fb717143b0a9b68bfdf6da19e520ddee
B Kleinen <kleinen@infrastructure.de>
2014-10-10 11:50:45 +0200

    class 20141009

---
+++ myfirstruby.rb
@@ -0,0 +1,21 @@
+
+class X
+	def sum(a,b)
+	  return a+b
+	  13
+    end
+end
+
+
+
+
+
+a = [1,2,4]
+
+puts "das zweite element ist: #{a[1]}"
+
+puts "reverse #{a.reverse}"
+@b =a 
+puts "reverse 2: #{@b.reverse}"
+
+puts X.new.sum(4,5)
